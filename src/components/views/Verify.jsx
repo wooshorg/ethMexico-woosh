2f317239111ba2bcbfe42c9d383ee1d7ec1bd0e1
paigexx <pcjones12@gmail.com>
2022-08-20 15:14:38 -0500

    worldcoin action/address

--- src/components/views/Verify.jsx
+++ src/components/views/Verify.jsx
@@ -1,8 +1,16 @@
 import { WorldIDWidget } from "@worldcoin/id"
+import { useContext, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { userContext } from "../../context/userContext"
+
 
 const Verify = () => {
     const navigate = useNavigate()
+    const {account} = useContext(userContext)
+
+    useEffect(() => {
+        console.log(account)
+    }, [])
 
 
     const completeVerification = (verificationResponse) => {
@@ -14,8 +22,8 @@ const Verify = () => {
         <>
             <h1>Looks your new here. Please verify your a hooman</h1>
             <WorldIDWidget
-                actionId="wid_BPZsRJANxct2cZxVRyh80SFG" // obtain this from developer.worldcoin.org
-                signal="my_signal"
+                actionId="wid_staging_1f0e5ad29850c3b9bdbb20a74476e547" // obtain this from developer.worldcoin.org
+                signal={account}
                 enableTelemetry
                 onSuccess={(verificationResponse) => completeVerification(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
                 onError={(error) => console.error(error)}
