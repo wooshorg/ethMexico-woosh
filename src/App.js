0e3d74fa2f1809de6d8e20281b03e7d5b2359696
Kenk <kenghin_lim@hotmail.com>
2022-08-21 10:16:17 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import { useState } from 'react';
-import { userContext } from './context/userContext';
-import { Routes, Route } from 'react-router-dom';
-import Landing from './components/views/Landing';
-import Home from './components/views/Home';
-import Verify from './components/views/Verify';
+import "./App.css";
 
-import Transactions from './components/transactions';
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./components/views/Home";
+import Landing from "./components/views/Landing";
+import Transactions from "./components/transactions";
+import Verify from "./components/views/Verify";
+import { useState } from "react";
+import { userContext } from "./context/userContext";
 
 function App() {
   const [account, setAccount] = useState(null);
@@ -17,9 +18,8 @@ function App() {
     <userContext.Provider value={value}>
       <div className="App">
         <Routes>
-          <Transactions />
-
           <Route path="/" element={<Landing />} />
+          <Route path="/transactions" element={<Transactions />} />
           <Route path="/home" element={<Home />} />
           <Route path="/verify" element={<Verify />} />
         </Routes>
--- src/services/web3_methods.js
+++ src/services/web3_methods.js
@@ -1,35 +1,41 @@
-    const sendTransaction = async () => {
-        const publicAddress = (await web3.eth.getAccounts())[0];
-        const txnParams = {
-        from: publicAddress,
-        to: publicAddress,
-        value: web3.utils.toWei("0.01", "ether"),
-        gasPrice: web3.utils.toWei("30", "gwei")
-        };
-        web3.eth
-        .sendTransaction(txnParams)
-        .on("transactionHash", (hash) => {
-            console.log("the txn hash that was returned to the sdk:", hash);
-        })
-        .then((receipt) => {
-            console.log("the txn receipt that was returned to the sdk:", receipt);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    };
+import web3 from "./web3";
+
+const daiContract = 
+
+const sendTransaction = async () => {
+  const publicAddress = (await web3.eth.getAccounts())[0];
+  const txnParams = {
+    from: publicAddress,
+    to: publicAddress,
+    value: web3.utils.toWei("0.01", "ether"),
+    gasPrice: web3.utils.toWei("30", "gwei"),
+  };
+  web3.eth
+    .sendTransaction(txnParams)
+    .on("transactionHash", (hash) => {
+      console.log("the txn hash that was returned to the sdk:", hash);
+    })
+    .then((receipt) => {
+      console.log("the txn receipt that was returned to the sdk:", receipt);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const sendDAI = async () => {
 
 
-    const signMessage = async () => {
-        const publicAddress = (await web3.eth.getAccounts())[0];
-        const signedMessage = await web3.eth.personal
-        .sign("My Message", publicAddress, "")
-        .catch((e) => console.log(e));
-        console.log(signedMessage);
-    };
+const signMessage = async () => {
+  const publicAddress = (await web3.eth.getAccounts())[0];
+  const signedMessage = await web3.eth.personal
+    .sign("My Message", publicAddress, "")
+    .catch((e) => console.log(e));
+  console.log(signedMessage);
+};
 
-    const showWallet = () => {
-        magic.connect.showWallet().catch((e) => {
-        console.log(e);
-        });
-    };
+const showWallet = () => {
+  magic.connect.showWallet().catch((e) => {
+    console.log(e);
+  });
+};
