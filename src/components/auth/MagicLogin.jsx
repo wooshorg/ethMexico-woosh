17350aca206956121130863efb9b51abcfdb7ebd
roy-sandoval <roy@roysandoval.com>
2022-08-20 21:40:31 -0500

    Fix merge issue

--- src/components/auth/MagicLogin.jsx
+++ src/components/auth/MagicLogin.jsx
@@ -6,49 +6,29 @@ import axios from 'axios';
 import Button from '../global/Button';
 
 const MagicLogin = () => {
-    const {setAccount} = useContext(userContext)
-    const navigate = useNavigate()
+  const { setAccount } = useContext(userContext);
+  const navigate = useNavigate();
 
-    const checkWorldID = async (address) => {
-        // Make a request for a user with a given ID
-        console.log('CheckworldId()', address)
-        axios.get(`https://woosh-backend.herokuapp.com/user/retrieve/${address}`)
-        .then(response =>  {
-            console.log("axios get", response);
-            if(response.data.worldcoin_hash !== undefined){
-                console.log("world coin hash: ", response.worldcoin_hash)
-                navigate("/home")
-            }
-            else{
-                navigate("/verify")
-            }
-        })
-        .catch(error => {
+  const checkWorldID = async (address) => {
+    // Make a request for a user with a given ID
+    console.log('CheckworldId()', address);
+    axios
+      .get(`https://woosh-backend.herokuapp.com/user/retrieve/${address}`)
+      .then((response) => {
+        console.log('axios get', response);
+        if (response.data.worldcoin_hash !== undefined) {
+          console.log('world coin hash: ', response.worldcoin_hash);
+          navigate('/home');
+        } else {
+          navigate('/verify');
+        }
+      })
+      .catch((error) => {
         // handle error
-            console.log("This is an error", error);
-            navigate("/verify")
-        })
-    }
-
-    const login = async () => {
-        web3.eth
-        .getAccounts()
-        .then((accounts) => {
-            setAccount(accounts?.[0]);
-            checkWorldID(accounts?.[0])
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    };
-    return (
-        <>
-            
-            <button onClick={login} className="button-row">
-                Enter Woosh
-            </button>
-        </>
-    )
+        console.log('This is an error', error);
+        navigate('/verify');
+      });
+  };
 
   const login = async () => {
     web3.eth
