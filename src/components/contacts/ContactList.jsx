ece4a78327877403742e2c111bf5a217917509e7
Temo <cramhd@gmail.com>
2022-08-21 04:43:13 -0500

    contact list (#14)

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,33 +1,46 @@
+import { useState, useEffect, useContext } from 'react';
+import { userContext } from '../../context/userContext';
+import { getContacts } from '../../lib/lens/getContacts';
+
 const ContactList = (props) => {
-  const contacts = [
-    {
-      user: 'user1.lens',
-      email: 'user@email.com',
-    },
-    {
-      user: 'user2.lens',
-      email: 'user@email.com',
-    },
-    {
-      user: 'user3.lens',
-      email: 'user@email.com',
-    },
-    {
-      user: 'user4.lens',
-      email: 'user@email.com',
-    },
-    {
-      user: 'user5.lens',
-      email: 'user@email.com',
-    },
-    {
-      user: 'user6.lens',
-      email: 'user@email.com',
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+  const { account } = useContext(userContext);
+
+  const loadContacts = async () => {
+    if (account === null) {
+      return;
+    }
+
+    const result = await getContacts(account);
+    const response = (result && result.following) || null;
+    const status = (response && response.__typename) || null;
+    let contactList = [];
+
+    if (status === "PaginatedFollowingResult") {
+      let contactList = [];
+      response.items.map((item) => {
+        const profile = item.profile;
+        contactList.push({
+          profileId: profile.profileId,
+          user: profile.handle,
+          picture: profile.picture && profile.picture.original && profile.picture.original.url || null,
+          wallet: profile.ownedBy,
+        });
+      });
+
+      setContacts((t) => {
+        return contactList;
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadContacts();
+  }, []);
+
   return (
     <div>
-      <h2 className="mb-6">Your contacts (48)</h2>
+      <h2 className="mb-6">Your contacts ({contacts.length})</h2>
       <div
         className={`flex gap-6 flex-col ${
           props.truncate ? 'h-[15rem]' : 'h-[30rem]'
@@ -38,7 +51,7 @@ const ContactList = (props) => {
             <div className="w-8 h-8 bg-white rounded-full" />
             <div className="flex flex-col">
               <span>{c.user}</span>
-              <span className="text-sm text-white/70">{c.email}</span>
+              <span className="text-sm text-white/70">{c.wallet}</span>
             </div>
           </div>
         ))}
