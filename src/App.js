673592c037de8ccb8a710f9b3b6271917447046a
Kenk <kenghin_lim@hotmail.com>
2022-08-21 15:24:13 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Home from "./components/views/Home";
 import Landing from "./components/views/Landing";
 import Profile from "./components/profile";
 import Send from "./components/views/Send";
-import Transactions from "./components/transactions";
 import Verify from "./components/views/Verify";
 import { useLocalStorage } from "./components/hooks/UseLocalStorage";
 import { useState } from "react";
 import { userContext } from "./context/userContext";
+
+// import Transactions from "./components/transactions";
 
 function App() {
   const [account, setAccount] = useLocalStorage("account", null);
@@ -22,7 +23,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/transactions" element={<Transactions />} />
+          {/* <Route path="/transactions" element={<Transactions />} /> */}
           <Route path="/home" element={<Home />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/send" element={<Send />} />
